f99bd4226b936f4a4a5bab3767b0e4f24cbbebd4
wurzel <wurzel@fb.com>
2017-01-12 09:07:04 -0800

    Add comments suitable for instructional code.

--- app/src/main/java/com/thekeirs/gameengine/games/FrogHerderLevel.java
+++ app/src/main/java/com/thekeirs/gameengine/games/FrogHerderLevel.java
@@ -1,37 +1,54 @@
 package com.thekeirs.gameengine.games;
 
 import com.thekeirs.gameengine.framework.Rand;
-import com.thekeirs.gameengine.system.FrogSprite;
 import com.thekeirs.gameengine.system.GameLevel;
 import com.thekeirs.gameengine.system.GameObject;
 import com.thekeirs.gameengine.system.Scene;
 import com.thekeirs.gameengine.system.SolidColorScene;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by wurzel on 1/6/17.
  */
 
 public class FrogHerderLevel extends GameLevel {
+    // WIDTH and HEIGHT set the size of the world the game will take place in.
     final int WIDTH = 1600;
     final int HEIGHT = 900;
+
+    // Configure the number of frogs present when the level starts
     final int NUM_FROGS = 5;
 
     public FrogHerderLevel() {
         super();
     }
 
+    // Configure the world, set up a background scene, and add the game objects
+    // into the world.
     public void setup() {
         mManager.setWorldScreenSize(WIDTH, HEIGHT);
+
+        // Set the background to a single solid color
         Scene scene = new SolidColorScene("#20c0ff");
-        // scene = new BackgroundImageScene(mManager, R.raw.my_background);
+
+        // If you want to make a custom background, create a new image, place it in
+        // the app/resources/raw folder with name my_background.png or my_background.jpg,
+        // and uncomment this line:
+        // Scene scene = new BackgroundImageScene(mManager, R.raw.my_background);
         mManager.setScene(scene);
 
         for (int i = 0; i < NUM_FROGS; ++i) {
-            int frogSize = Rand.between(80, 150);
-            GameObject frog = new FrogSprite("frog" + i, Rand.between(0, WIDTH - 30), Rand.between(0, HEIGHT - 30), frogSize, frogSize);
+            // Since the world is 900 units tall, 100 unit tall frogs means we can fit 9 frogs
+            // high on the screen.
+            int frogSize = 100;
+
+            // Create a frog named frog0, frog1, etc, at a random location with the requested size
+            GameObject frog = new FrogSprite("frog" + i,
+                    Rand.between(0, WIDTH - frogSize),
+                    Rand.between(0, HEIGHT - frogSize),
+                    frogSize, frogSize);
+
+            // Add the frog to the Game Object Manager so it can be drawn on the screen,
+            // touched, and have its location updated.
             mManager.addObject(frog);
         }
     }
--- app/src/main/java/com/thekeirs/gameengine/games/FrogSprite.java
+++ app/src/main/java/com/thekeirs/gameengine/games/FrogSprite.java
@@ -1,14 +1,22 @@
-package com.thekeirs.gameengine.system;
+package com.thekeirs.gameengine.games;
 
 import com.thekeirs.gameengine.R;
 import com.thekeirs.gameengine.framework.Rand;
+import com.thekeirs.gameengine.system.Audio;
+import com.thekeirs.gameengine.system.Sprite;
 
 /**
- * Created by wurzel on 12/30/16.
+ * Created by wurzel on 2016-Dec-20
+ *
+ * This class uses Sprite (which draws an image on the screen at a requested location)
+ * and adds two additional behaviors:
+ *    in update() the frog may randomly hop around every once in a while
+ *    in onTouch() the frog emits a noise and hops in a certain direction when tapped on the screen
  */
 
 public class FrogSprite extends Sprite {
     final private static String TAG = "FrogSprite";
+
 
     public FrogSprite(String name, float x, float y, float width, float height) {
         super(name, x, y, width, height);
@@ -20,12 +28,19 @@ public class FrogSprite extends Sprite {
         if (!imageLoaded()) {
             return;
         }
+        // onceEvery(N) does something randomly approximately every N seconds.
+        // The "f" in "2.0f" means it's a floating point number.
         if (Rand.onceEvery(2.0f)) {
+            // Choose random distance and angle to hop.
             int dist = Rand.between(30, 50);
             float direction = (float) Math.toRadians(Rand.between(0, 360));
+            // Move the sprite in the desired direction
             hop(dist, direction);
         }
 
+        // We don't want the frog to hop off the edge of the screen, so this mess of
+        // conditionals checks if the frog's coordinates are outside our world size and
+        // forces them back inside.
         if (boundingRect.left < 0) {
             boundingRect.offsetTo(0, boundingRect.top);
         } else if (boundingRect.right > manager.mWorldScreenWidth) {
@@ -41,11 +56,13 @@ public class FrogSprite extends Sprite {
 
     @Override
     public void onTouch(float x, float y) {
+        // Pick a random distance to hop when tapped.
         int dist = Rand.between(30, 50);
 
+        // When tapped, always hop exactly towards the middle of the screen.
         hopToward(dist, manager.mWorldScreenWidth / 2.0f, manager.mWorldScreenHeight / 2.0f);
 
-        manager.mBus.postMessage(new Message("ribbit"));
+        // And make a suitable frog noise.
         Audio.play(R.raw.frog_croak);
     }
 }
